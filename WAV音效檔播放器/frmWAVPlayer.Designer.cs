ba057fbbfb9e0e29c56430d1eef79c01cb84e135
sixian870 <alice0701lee@gmail.com>
2026-05-10 13:13:45 +0800

    加入專案檔案。

---
+++ WAV音效檔播放器/frmWAVPlayer.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WAV音效檔播放器
+{
+    partial class frmWAVPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpPath = new System.Windows.Forms.GroupBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.ofdWAVFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSave = new System.Windows.Forms.SaveFileDialog();
+            this.grpPath.SuspendLayout();
+            this.grpButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPath
+            // 
+            this.grpPath.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.grpPath.Controls.Add(this.btnBrowse);
+            this.grpPath.Controls.Add(this.txtPath);
+            this.grpPath.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpPath.ForeColor = System.Drawing.Color.Honeydew;
+            this.grpPath.Location = new System.Drawing.Point(18, 17);
+            this.grpPath.Name = "grpPath";
+            this.grpPath.Size = new System.Drawing.Size(559, 85);
+            this.grpPath.TabIndex = 0;
+            this.grpPath.TabStop = false;
+            this.grpPath.Text = "音效位置";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Honeydew;
+            this.btnBrowse.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowse.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnBrowse.Location = new System.Drawing.Point(436, 31);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(97, 31);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "瀏覽";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtPath.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPath.Location = new System.Drawing.Point(24, 34);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(406, 25);
+            this.txtPath.TabIndex = 0;
+            // 
+            // grpButton
+            // 
+            this.grpButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.grpButton.Controls.Add(this.btnConvert);
+            this.grpButton.Controls.Add(this.btnEnd);
+            this.grpButton.Controls.Add(this.btnStop);
+            this.grpButton.Controls.Add(this.btnPlay);
+            this.grpButton.Controls.Add(this.btnLoop);
+            this.grpButton.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpButton.ForeColor = System.Drawing.Color.Honeydew;
+            this.grpButton.Location = new System.Drawing.Point(18, 112);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Size = new System.Drawing.Size(559, 83);
+            this.grpButton.TabIndex = 1;
+            this.grpButton.TabStop = false;
+            this.grpButton.Text = "播放按鈕";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Honeydew;
+            this.btnConvert.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnConvert.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnConvert.Location = new System.Drawing.Point(333, 31);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(97, 31);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "轉換格式";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.BackColor = System.Drawing.Color.Honeydew;
+            this.btnEnd.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnEnd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEnd.Location = new System.Drawing.Point(436, 31);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(97, 31);
+            this.btnEnd.TabIndex = 6;
+            this.btnEnd.Text = "結束程式";
+            this.btnEnd.UseVisualStyleBackColor = false;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Honeydew;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnStop.Location = new System.Drawing.Point(230, 31);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(97, 31);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "停止播放";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Honeydew;
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnPlay.Location = new System.Drawing.Point(24, 31);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(97, 31);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放一次";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.BackColor = System.Drawing.Color.Honeydew;
+            this.btnLoop.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLoop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLoop.Location = new System.Drawing.Point(127, 31);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(97, 31);
+            this.btnLoop.TabIndex = 3;
+            this.btnLoop.Text = "重複播放";
+            this.btnLoop.UseVisualStyleBackColor = false;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // ofdWAVFile
+            // 
+            this.ofdWAVFile.DefaultExt = "wav";
+            this.ofdWAVFile.Title = "開啟檔案位置";
+            // 
+            // sfdSave
+            // 
+            this.sfdSave.Title = "儲存檔案位置";
+            // 
+            // frmWAVPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(595, 212);
+            this.Controls.Add(this.grpButton);
+            this.Controls.Add(this.grpPath);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmWAVPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WAV音效檔播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmWAVPlayer_FormClosing);
+            this.grpPath.ResumeLayout(false);
+            this.grpPath.PerformLayout();
+            this.grpButton.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.OpenFileDialog ofdWAVFile;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.SaveFileDialog sfdSave;
+    }
+}
+
---
+++ WAV音效檔播放器/frmWAVPlayer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media; // 引用System.Media命名空間
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave; // 引用NAudio命名空間
+using NReco.VideoConverter; // 引用NReco.VideoConverter命名空間
+using NAudio.Lame; // 引用NAudio.Lame命名空間
+
+namespace WAV音效檔播放器
+{
+    public partial class frmWAVPlayer : Form
+    {
+        SoundPlayer player; // 宣告SoundPlayer物件
+
+        public frmWAVPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ofdWAVFile.Filter = "WAV Files(*.wav)|*.wav";
+            // 打開檔案對話方塊
+            if (ofdWAVFile.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofdWAVFile.FileName;
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            // 若沒有輸入
+            if (string.IsNullOrWhiteSpace(txtPath.Text) || !File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇有效的 WAV 檔案！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                if (player != null) player.Stop(); // 先停止當前播放
+                player = new SoundPlayer(); // 建立播放器物件
+                player.SoundLocation = txtPath.Text; // 指定音效所在路徑檔名
+                player.Load(); // 載入音效檔資料
+                player.Play(); // 播放音效檔
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("無法播放音效檔 : "+ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+            // 若沒有輸入
+            if (string.IsNullOrWhiteSpace(txtPath.Text) || !File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇有效的 WAV 檔案！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                if (player != null) player.Stop(); // 先停止當前播放
+                player = new SoundPlayer(txtPath.Text);
+                player.PlayLooping();// 重複播放
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("無法播放音效檔 : " + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (player != null)
+            {
+                player.Stop();
+            }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmWAVPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("確定要關閉應用程式嗎？", "關閉確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true; // 取消關閉
+            }
+        }
+
+        #region 轉檔功能
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // 檢查是否有輸入有效的 WAV 檔案
+            if (string.IsNullOrWhiteSpace(txtPath.Text) || !File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇要轉換的有效 WAV 檔案！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (Path.GetExtension(txtPath.Text).ToLower() != ".wav")
+            {
+                MessageBox.Show("請確定選擇的是 WAV 檔案！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // MP3, AAC, WMA, FLAC
+            sfdSave.Filter = "MP3 檔案 (*.mp3)|*.mp3|AAC 檔案 (*.m4a)|*.m4a|WMA 檔案 (*.wma)|*.wma|FLAC 無損音訊 (*.flac)|*.flac";
+            sfdSave.FilterIndex = 1; // 預設為 MP3
+
+            sfdSave.FileName = Path.GetFileNameWithoutExtension(txtPath.Text);
+
+            if (sfdSave.ShowDialog() == DialogResult.OK)
+            {
+                // 暫停播放，釋放音效檔佔用
+                if (player != null) player.Stop();
+
+                this.Text = "WAV音效檔播放器 - 轉檔中請稍候...";
+                Application.DoEvents(); // 強制刷新畫面
+
+                try
+                {
+                    // NAudio 支援 MP3, AAC, WMA 格式轉換，FLAC 需要使用 NReco (底層為 FFmpeg) 處理
+                    if (sfdSave.FilterIndex >= 1 && sfdSave.FilterIndex <= 3)
+                    {
+                        // 選項 1, 2, 3 用 NAudio
+                        using (var reader = new AudioFileReader(txtPath.Text))
+                        {
+                            switch (sfdSave.FilterIndex)
+                            {
+                                case 1: // MP3
+                                    using (var writer = new LameMP3FileWriter(sfdSave.FileName, reader.WaveFormat, 128))
+                                    {
+                                        reader.CopyTo(writer);
+                                    }
+                                    break;
+                                case 2: // AAC
+                                    MediaFoundationEncoder.EncodeToAac(reader, sfdSave.FileName);
+                                    break;
+                                case 3: // WMA
+                                    MediaFoundationEncoder.EncodeToWma(reader, sfdSave.FileName);
+                                    break;
+                            }
+                        }
+                    }
+                    else if (sfdSave.FilterIndex == 4)
+                    {
+                        // 選項 4 FLAC 用 NReco (底層為 FFmpeg) 處理
+                        var ffMpeg = new FFMpegConverter();
+                        ffMpeg.ConvertMedia(txtPath.Text, sfdSave.FileName, "flac");
+                    }
+
+                    MessageBox.Show("轉檔成功！\n檔案已儲存至：\n" + sfdSave.FileName, "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("轉檔失敗 : " + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    // 復原標題文字
+                    this.Text = "WAV音效檔播放器";
+                }
+            }
+        }
+        #endregion
+    }
+}
